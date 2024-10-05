9524092f6a192607c9aed631d05b1829691d9330
ma4z <164923658+ma4z@users.noreply.github.com>
2024-10-05 05:11:51 -0700

    W eror

--- app/servers.js
+++ app/servers.js
@@ -113,96 +113,99 @@ router.get('/delete', ensureAuthenticated, async (req, res) => {
 
 // Create server
 router.get('/create', ensureAuthenticated, async (req, res) => {
-  if (!req.user || !req.user.email || !req.user.id) return res.redirect('/login/discord');
-  if (!req.query.name || !req.query.node || !req.query.image || !req.query.cpu || !req.query.ram) return res.redirect('../create-server?err=MISSINGPARAMS');
+    if (!req.user || !req.user.email || !req.user.id) return res.redirect('/login/discord');
+    if (!req.query.name || !req.query.node || !req.query.image || !req.query.cpu || !req.query.ram) 
+        return res.redirect('../create-server?err=MISSINGPARAMS');
 
-  // Resource checks
-  const max = await maxResources(req.user.email);
-  const existing = await existingResources(req.user.id);
-  if (parseInt(req.query.cpu) > parseInt(max.cpu - existing.cpu)) return res.redirect('../create-server?err=NOTENOUGHRESOURCES');
-  if (parseInt(req.query.ram) > parseInt(max.ram - existing.ram)) return res.redirect('../create-server?err=NOTENOUGHRESOURCES');
+    // Resource checks
+    const max = await maxResources(req.user.email);
+    const existing = await existingResources(req.user.id);
+    if (parseInt(req.query.cpu) > parseInt(max.cpu - existing.cpu)) 
+        return res.redirect('../create-server?err=NOTENOUGHRESOURCES');
+    if (parseInt(req.query.ram) > parseInt(max.ram - existing.ram)) 
+        return res.redirect('../create-server?err=NOTENOUGHRESOURCES');
 
-  if (parseInt(req.query.ram) < 128) return res.redirect('../create-server?err=INVALID');
-  if (parseInt(req.query.cpu) < 0) return res.redirect('../create-server?err=INVALID');
+    if (parseInt(req.query.ram) < 128) return res.redirect('../create-server?err=INVALID');
+    if (parseInt(req.query.cpu) < 0) return res.redirect('../create-server?err=INVALID');
 
-  if (req.query.name.length > 100 || req.query.name.length < 3) return res.redirect('../create-server?err=INVALID');
-  if (isNaN(req.query.cpu) || isNaN(req.query.ram)) return res.redirect('../create-server?err=INVALID');
+    if (req.query.name.length > 100 || req.query.name.length < 3) 
+        return res.redirect('../create-server?err=INVALID');
+    if (isNaN(req.query.cpu) || isNaN(req.query.ram)) 
+        return res.redirect('../create-server?err=INVALID');
 
-  try {
-      const userId = await db.get(`id-${req.user.email}`);
-      const name = req.query.name;
-      const nodeId = req.query.node;
-      const imageId = req.query.image;
-      const cpu = parseInt(req.query.cpu);
-      const memory = parseInt(req.query.ram);
-      const imagename = req.query.imageName; 
+    try {
+        const userId = await db.get(`id-${req.user.email}`);
+        const name = req.query.name;
+        const nodeId = req.query.node;
+        const imageId = req.query.image;
+        const cpu = parseInt(req.query.cpu);
+        const memory = parseInt(req.query.ram);
+        const imagename = req.query.imageName;
 
-      // Parse JSON variables
-      const variables = JSON.parse(decodeURIComponent(req.query.variables || '{}'));
+        // Parse JSON variables
+        const variables = JSON.parse(decodeURIComponent(req.query.variables || '{}'));
 
-      const portsData  = require('../storage/ports.json');
-      const selectedPortKey = getRandomPort(portsData.portAvailable);
-      const selectedPort = portsData.portAvailable[selectedPortKey];
+        const portsData = require('../storage/ports.json');
+        const selectedPortKey = getRandomPort(portsData.portAvailable);
+        const selectedPort = portsData.portAvailable[selectedPortKey];
 
-      if (!selectedPort || !selectedPortKey) {
-          console.error('No ports available');
-          res.redirect('../create-server?err=NOPORTAVAILABLE');
-      }
+        if (!selectedPort || !selectedPortKey) {
+            console.error('No ports available');
+            return res.redirect('../create-server?err=NOPORTAVAILABLE');
+        }
 
-      portsData.portInUse[selectedPortKey] = selectedPort;
-      delete portsData.portAvailable[selectedPortKey];
+        portsData.portInUse[selectedPortKey] = selectedPort;
+        delete portsData.portAvailable[selectedPortKey];
 
-      fs.writeFileSync('./storage/ports.json', JSON.stringify(portsData, null, 2), 'utf-8');
+        fs.writeFileSync('./storage/ports.json', JSON.stringify(portsData, null, 2), 'utf-8');
 
-      const images = require('../storage/images.json');
-      const image2 = images.find(image => image.Image === imageId);
-      if (!image2) return res.redirect('../create-server?err=INVALID_IMAGE');
-      const image = image2.Image;
+        const images = require('../storage/images.json');
+        const image2 = images.find(image => image.Image === imageId);
+        if (!image2) return res.redirect('../create-server?err=INVALID_IMAGE');
+        const image = image2.Image;
 
-      await axios.post(`${skyport.url}/api/instances/deploy`, {
-       image,
-       imagename, 
-       memory,
-       cpu,
-       ports: selectedPort,
-       nodeId,
-       name,
-       user: userId,
-       variables
-      }, {
-       headers: {
-         'x-api-key': skyport.key
-       }
-   }).then(async (response) => {
-       const serverId = response.data.Id; 
-       const createTableQuery = `
-           CREATE TABLE server_${serverId} (
-               name TEXT,
-               cpu INTEGER,
-               memory INTEGER,
-               image TEXT,
-               variables JSON
-           )
-       `;
-   });
+        const response = await axios.post(`${skyport.url}/api/instances/deploy`, {
+            image,
+            imagename, 
+            memory,
+            cpu,
+            ports: selectedPort,
+            nodeId,
+            name,
+            user: userId,
+            variables
+        }, {
+            headers: {
+                'x-api-key': skyport.key
+            }
+        });
 
+        const serverId = response.data.Id; 
+        const createTableQuery = `
+            CREATE TABLE server_${serverId} (
+                name TEXT,
+                cpu INTEGER,
+                memory INTEGER,
+                image TEXT,
+                variables JSON
+            )
+        `;
 
-          await db.run(createTableQuery);
-          const insertValuesQuery = `
-              INSERT INTO server_${serverId} (name, cpu, memory, image, variables)
-              VALUES (?, ?, ?, ?, ?)
-          `;
+        await db.run(createTableQuery);
+        const insertValuesQuery = `
+            INSERT INTO server_${serverId} (name, cpu, memory, image, variables)
+            VALUES (?, ?, ?, ?, ?)
+        `;
 
-          await db.run(insertValuesQuery, [name, cpu, memory, imageId, JSON.stringify(variables)]);
+        await db.run(insertValuesQuery, [name, cpu, memory, imageId, JSON.stringify(variables)]);
 
-          res.redirect('/servers?err=CREATED');
-          await sendDiscordNotification(`${req.user.email} Have Created a Server with:\n**CPU**: ${cpu}\n**RAM**: ${memory}\nName: ${name}.`);
-      });
+        res.redirect('/servers?err=CREATED');
+        await sendDiscordNotification(`${req.user.email} Have Created a Server with:\n**CPU**: ${cpu}\n**RAM**: ${memory}\nName: ${name}.`);
 
-  } catch (error) {
-      console.error(error);
-      res.redirect('../create-server?err=ERRORONCREATE');
-  }
+    } catch (error) {
+        console.error(error);
+        res.redirect('../create-server?err=ERRORONCREATE');
+    }
 });
 
 
